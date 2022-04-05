0afb968c3b7e7b1260da5e3c9be5d1b565d4acb1
dante-sarmiento <dante.sarmientof5@gmail.com>
2022-04-05 15:50:34 -0300

    inicializando backend de la app, nodemom, server and db connection

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var app = express();
+var port = 3000;
+const password = 'devaccount123';
+var URL = `mongodb+srv://DanteDev:${password}@cluster0.fwrz0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+var mongoose = require('mongoose')
 
-app.listen(3000, () =>{
-    console.log('Servidor esta corriendo')
-})
+async function connect() {
+    try {
+    await mongoose.connect(URL);
+    console.log('\x1b[36m connected to mongoDB \x1b[37m');
+    app.listen(port, () => {
+        console.log(`\x1b[36m Server started on port: ${port} \x1b[37m`);
+        });
+    }
+    catch(error) {
+        console.log('\x1b[31m Error al conectar con MongoDB \x1b[37m');
+    } 
+}
+connect();
