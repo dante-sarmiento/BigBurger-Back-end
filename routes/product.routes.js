87a6619e433010f138da37c4b61c0e0f383cea47
Wenceslao José Villegas <wenceslaovillegas14@gmail.com>
2022-04-08 19:29:08 -0300

    Product routes

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,2 +1,11 @@
 var express = require("express")
 var api = express.Router()
+var productController = require("../controllers/product.controller")
+
+api.post("/product", productController.addProducts)
+api.get("/products", productController.getProducts)
+api.get("/product", productController.getProduct)
+api.delete("/product/", productController.deleteProduct)
+api.put("/product/upd_id", productController.updateProduct)
+
+module.exports = api
