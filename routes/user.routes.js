28134f115f7bd06d502c9145902ed80fb0c15078
Ricardo Ramos <indricardoramos@gmail.com>
2022-04-17 12:50:03 -0300

    cambio app por api en routes, no pusheo

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,18 @@
 var express = require('express');
-var app = express.Router();
+var api = express.Router();
 var userController = require('../controllers/user.controllers');
 
 
-app.post('/user', userController.addUser);
+api.post('/user', userController.addUser);
 
-app.get('/users', userController.getUsers);
+api.get('/users', userController.getUsers);
 
-app.get('/user', userController.getUser);
+api.get('/user', userController.getUser);
 
-app.delete('/user', userController.deleteUser);
+api.delete('/user', userController.deleteUser);
 
-app.put('/user/:upd_id', userController.updateUser);
+api.put('/user/:upd_id', userController.updateUser);
 
-app.post('/login', userController.login);
+api.post('/login', userController.login);
 
-module.exports = app;
+module.exports = api;
