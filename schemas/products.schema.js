f2bbf0c8781fae6ff3fcf8a63f24f06bc9bb42c6
Wenceslao José Villegas <wenceslaovillegas14@gmail.com>
2022-05-08 19:45:40 -0300

    merge dev

---
+++ schemas/products.schema.js
@@ -0,0 +1,64 @@
+var mongoose = require("mongoose")
+var Schema = mongoose.Schema
+
+var Categories = [
+    "Hamburguesas",
+    "Simples",
+    "Dobles",
+    "Triples",
+    "Vegetarianas",
+]
+
+var ProductSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 40,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 250,
+    },
+    price: {
+        type: Number,
+        required: true,
+
+        min: 0,
+        max: 10000
+
+    },
+    stock: {
+        type: Boolean,
+        default: true,
+    },
+    categorie_id: {
+        type: String,
+        enum: Categories,
+        default: "Hamburguesas",
+        required: true,
+    },
+
+    IMG: {
+        type: String,
+        required: true,
+        default: "unknow"
+    },
+
+    cretatedAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date
+    },
+    clientId: {
+        type: String,
+        ref: "User",
+        default: "Unknow"
+
+    }
+})
+
+module.exports = mongoose.model("Product", ProductSchema)
