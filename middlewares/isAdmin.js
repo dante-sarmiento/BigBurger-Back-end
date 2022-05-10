537501cb23b39db0afce8398aa45ff633fe17899
Wenceslao José Villegas <wenceslaovillegas14@gmail.com>
2022-05-10 15:54:22 -0300

    agregado middleware a products y correccion en isAdmin

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,5 +1,5 @@
 const isAdmin = (req, res, next) => {
-    if(req.user.role !== 'ADMIN_ROLE') return res.status(401).send('no tiene los permisos necesarios para borrar un usuario');
+    if(req.user.role !== 'ADMINISTRADOR') return res.status(401).send('no tiene los permisos necesarios para borrar un usuario');
     next();
 }
 
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,11 +1,13 @@
 var express = require("express")
 var api = express.Router()
 var productController = require("../controllers/product.controller")
+const checkAuthentication = require('../middlewares/authentication')
+const isAdmin = require('../middlewares/isAdmin')
 
-api.post("/product", productController.addProducts)
+api.post("/product", [checkAuthentication, isAdmin] ,productController.addProducts)
 api.get("/products", productController.getProducts)
 api.get("/product", productController.getProduct)
-api.delete("/product/", productController.deleteProduct)
-api.put("/product/upd_id", productController.updateProduct)
+api.delete("/product/", [checkAuthentication, isAdmin] ,productController.deleteProduct)
+api.put("/product/upd_id", [checkAuthentication, isAdmin] ,productController.updateProduct)
 
 module.exports = api
